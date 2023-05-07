83337d6935b57cd3a5d892afbb393aca6b1e7ece
Archer <ogunkapc@gmail.com>
2023-05-07 19:33:51 +0100

    add show password icon

--- lib/screens/register_screen.dart
+++ lib/screens/register_screen.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:mynotes/firebase_options.dart';
 import 'package:mynotes/screens/login_screen.dart';
@@ -14,6 +15,7 @@ class RegisterationScreen extends StatefulWidget {
 class _RegisterationScreenState extends State<RegisterationScreen> {
   late final TextEditingController _email;
   late final TextEditingController _password;
+  bool isSelectionOn = true;
 
   @override
   void initState() {
@@ -43,133 +45,158 @@ class _RegisterationScreenState extends State<RegisterationScreen> {
         builder: (context, snapshot) {
           switch (snapshot.connectionState) {
             case ConnectionState.done:
-              return Column(
-                children: [
-                  TextField(
-                    controller: _email,
-                    enableSuggestions: false,
-                    autocorrect: false,
-                    keyboardType: TextInputType.emailAddress,
-                    decoration: const InputDecoration(
-                        hintText: "Enter your email here"),
-                  ),
-                  const SizedBox(
-                    height: 10,
-                  ),
-                  TextField(
-                    controller: _password,
-                    obscureText: true,
-                    enableSuggestions: false,
-                    autocorrect: false,
-                    decoration: const InputDecoration(
-                        hintText: "Enter your password here"),
-                  ),
-                  TextButton(
-                    onPressed: () async {
-                      final email = _email.text;
-                      final password = _password.text;
-                      try {
-                        final userCredential = await FirebaseAuth.instance
-                            .createUserWithEmailAndPassword(
-                          email: email,
-                          password: password,
-                        );
-                        print(userCredential);
-                      } on FirebaseAuthException catch (e) {
-                        print(e);
-                        if (e.code == "email-already-in-use") {
-                          print("Email already in use");
-                          showDialog(
-                            context: context,
-                            builder: (context) => Container(
-                              padding: const EdgeInsets.all(20),
-                              margin: const EdgeInsets.symmetric(
-                                  horizontal: 70, vertical: 300),
-                              color: Colors.white,
-                              child: Column(
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                children: const [
-                                  Text(
-                                    "Email already in use",
-                                    style: TextStyle(
-                                        fontSize: 20,
-                                        fontWeight: FontWeight.w600),
-                                  ),
-                                ],
-                              ),
-                            ),
-                          );
-                        } else if (e.code == "weak-password") {
-                          print("Weak password");
-                          showDialog(
-                            context: context,
-                            builder: (context) => Container(
-                              padding: const EdgeInsets.all(20),
-                              margin: const EdgeInsets.symmetric(
-                                  horizontal: 70, vertical: 300),
-                              color: Colors.white,
-                              child: Column(
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                children: const [
-                                  Text(
-                                    "Weak password",
-                                    style: TextStyle(
-                                        fontSize: 20,
-                                        fontWeight: FontWeight.w600),
-                                  ),
-                                ],
-                              ),
-                            ),
-                          );
-                        } else if (e.code == "invalid-email") {
-                          print("Invalid email");
-                          showDialog(
-                            context: context,
-                            builder: (context) => Container(
-                              padding: const EdgeInsets.all(20),
-                              margin: const EdgeInsets.symmetric(
-                                  horizontal: 70, vertical: 300),
-                              color: Colors.white,
-                              child: Column(
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                children: const [
-                                  Text(
-                                    "Invalid email",
-                                    style: TextStyle(
-                                        fontSize: 20,
-                                        fontWeight: FontWeight.w600),
-                                  ),
-                                ],
-                              ),
-                            ),
-                          );
-                        }
-                      }
-                    },
-                    child: const Text("Register"),
-                  ),
-                  const SizedBox(
-                    height: 10,
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      const Text("Already have an account? "),
-                      GestureDetector(
-                        onTap: () {
-                          Navigator.pushReplacement(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) => const LoginScreen()));
-                        },
-                        child: const Text(
-                          "Login",
-                          style: TextStyle(color: Colors.blue),
+              return Padding(
+                padding: const EdgeInsets.only(top: 8.0, left: 8.0, right: 8.0),
+                child: Column(
+                  children: [
+                    TextField(
+                      controller: _email,
+                      enableSuggestions: false,
+                      autocorrect: false,
+                      keyboardType: TextInputType.emailAddress,
+                      decoration: InputDecoration(
+                        hintText: "Enter your email here",
+                        filled: true,
+                        fillColor: Colors.blue.shade50,
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(20),
                         ),
                       ),
-                    ],
-                  )
-                ],
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    TextField(
+                      controller: _password,
+                      obscureText: isSelectionOn,
+                      enableSuggestions: false,
+                      autocorrect: false,
+                      decoration: InputDecoration(
+                        hintText: "Enter your password here",
+                        filled: true,
+                        fillColor: Colors.blue.shade50,
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                        suffixIcon: GestureDetector(
+                          onTap: () => setState(
+                            () {
+                              isSelectionOn = !isSelectionOn;
+                            },
+                          ),
+                          child: Icon(isSelectionOn
+                              ? CupertinoIcons.eye_slash_fill
+                              : CupertinoIcons.eye_fill),
+                        ),
+                      ),
+                    ),
+                    TextButton(
+                      onPressed: () async {
+                        final email = _email.text;
+                        final password = _password.text;
+                        try {
+                          final userCredential = await FirebaseAuth.instance
+                              .createUserWithEmailAndPassword(
+                            email: email,
+                            password: password,
+                          );
+                          print(userCredential);
+                        } on FirebaseAuthException catch (e) {
+                          print(e);
+                          if (e.code == "email-already-in-use") {
+                            print("Email already in use");
+                            showDialog(
+                              context: context,
+                              builder: (context) => Container(
+                                padding: const EdgeInsets.all(20),
+                                margin: const EdgeInsets.symmetric(
+                                    horizontal: 70, vertical: 300),
+                                color: Colors.white,
+                                child: Column(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: const [
+                                    Text(
+                                      "Email already in use",
+                                      style: TextStyle(
+                                          fontSize: 20,
+                                          fontWeight: FontWeight.w600),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            );
+                          } else if (e.code == "weak-password") {
+                            print("Weak password");
+                            showDialog(
+                              context: context,
+                              builder: (context) => Container(
+                                padding: const EdgeInsets.all(20),
+                                margin: const EdgeInsets.symmetric(
+                                    horizontal: 70, vertical: 300),
+                                color: Colors.white,
+                                child: Column(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: const [
+                                    Text(
+                                      "Weak password",
+                                      style: TextStyle(
+                                          fontSize: 20,
+                                          fontWeight: FontWeight.w600),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            );
+                          } else if (e.code == "invalid-email") {
+                            print("Invalid email");
+                            showDialog(
+                              context: context,
+                              builder: (context) => Container(
+                                padding: const EdgeInsets.all(20),
+                                margin: const EdgeInsets.symmetric(
+                                    horizontal: 70, vertical: 300),
+                                color: Colors.white,
+                                child: Column(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: const [
+                                    Text(
+                                      "Invalid email",
+                                      style: TextStyle(
+                                          fontSize: 20,
+                                          fontWeight: FontWeight.w600),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            );
+                          }
+                        }
+                      },
+                      child: const Text("Register"),
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        const Text("Already have an account? "),
+                        GestureDetector(
+                          onTap: () {
+                            Navigator.pushReplacement(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => const LoginScreen()));
+                          },
+                          child: const Text(
+                            "Login",
+                            style: TextStyle(color: Colors.blue),
+                          ),
+                        ),
+                      ],
+                    )
+                  ],
+                ),
               );
             default:
               return const Text("Loading...");
