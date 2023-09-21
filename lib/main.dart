b0f2af858ac387512b9a9d6d15aa480fa06618de
Archer <ogunkapc@gmail.com>
2023-09-21 17:43:27 +0100

    update auth screen routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:mynotes/auth/login_screen.dart';
 import 'package:mynotes/auth/register_screen.dart';
+import 'package:mynotes/auth/verify_email.dart';
 import 'package:mynotes/firebase_options.dart';
 
 void main() {
@@ -42,14 +44,17 @@ class HomePage extends StatelessWidget {
       builder: (context, snapshot) {
         switch (snapshot.connectionState) {
           case ConnectionState.done:
-            // final user = FirebaseAuth.instance.currentUser;
-            // if (user?.emailVerified ?? false) {
-            //   return const Text("Done");
-            // } else {
-            //   print("Not verified!!!");
-            //   return const VerifyEmailView();
-            // }
-            return const LoginScreen();
+            final user = FirebaseAuth.instance.currentUser;
+            if (user != null) {
+              if (user.emailVerified) {
+                print("Email is verified");
+              } else {
+                return const VerifyEmailView();
+              }
+            } else {
+              return const LoginScreen();
+            }
+            return const Text("Done");
           default:
             return const CircularProgressIndicator();
         }
